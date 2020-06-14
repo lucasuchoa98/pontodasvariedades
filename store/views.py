c80e799394db11271a27a6f5425ba599cdc14d0d
lucasuchoa98 <38196129+lucasuchoa98@users.noreply.github.com>
2020-06-13 21:42:58 -0300

    Add files via upload

--- store/views.py
+++ store/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from .models import *
 from .utils import cookieCart, cartData, guestOrder
@@ -7,6 +6,10 @@ import json
 import datetime
 from django.db.models import Q
 from allauth.account.decorators import login_required
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 
 # Create your views here.
 """
@@ -23,18 +26,6 @@ class StoreView(ListView):
         return context
 """
 
-"""class ContatoView(TemplateView):
-    template_name = "store/contato.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        data = cartData(self.request)
-        
-        cartItems = data['cartItems']
-        context['cartItems'] = cartItems
-
-        return context
-"""
 def store(request):
     data = cartData(request)
     cartItems = data['cartItems']
@@ -87,15 +78,6 @@ def checkout(request):
     context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'store/checkout.html', context)
 
-"""
-def itemDetail(request):
-    data = cartData(request)
-    product = Product.get_slug_field()
-
-    cartItems = data['cartItems']
-    context = {'items':items, 'cartItems':cartItems}
-    return render(request, 'store/product.html', context)
-"""
 class ItemDetailView(DetailView):
     model = Product
     template_name = "store/product.html"
@@ -173,3 +155,56 @@ def processOrder(request):
                 zipcode=data['shipping']['zipcode'],
             )
     return JsonResponse('Pagamento completo', safe=False)
+
+from django.core.mail import EmailMessage
+from django.shortcuts import redirect
+from django.template.loader import get_template
+
+def contact(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
+    context = {'cartItems':cartItems}
+    form_class = ContactForm
+
+    if request.method == 'POST':
+        form = form_class(data=request.POST)
+
+        if form.is_valid():
+            contact_name = request.POST.get(
+                'contact_name'
+            , '')
+            contact_email = request.POST.get(
+                'contact_email'
+            , '')
+            form_content = request.POST.get('content', '')
+
+            Subject = request.POST.get('subject', '')
+
+            # Email the profile with the
+            # contact information
+            template =  get_template('store/contact_template.txt')
+            context = {
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'form_content': form_content,
+            }
+            content = template.render(context)
+
+            email = EmailMessage(
+                "New contact form submission",
+                content,
+                "Ponto das variedades" +'',
+                ['lucasuchoalg@gmail.com'],
+                headers = {'Reply-To': contact_email }
+            )
+            email.send()
+
+            send_mail(Subject, form_content, contact_email, ['lucasuchoalg@gmail.com'], fail_silently=False)
+            return redirect('store:contato')
+
+    return render(request, 'store/contato.html', {
+        'form': form_class,
+    })
+
+def successView(request):
+    return JsonResponse('obrigado pela mensagem')
