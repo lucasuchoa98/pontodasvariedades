56f43277dec34bc555a87d02c4d04abd10a580ac
lucasuchoa98 <38196129+lucasuchoa98@users.noreply.github.com>
2020-06-13 21:45:30 -0300

    Add files via upload

--- store/forms.py
+++ store/forms.py
@@ -1,16 +1,20 @@
 from allauth.account.forms import SignupForm
 from django import forms
 from .models import *
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
 
-class CustomSignupForm(SignupForm):
-
-    def save(self, request):
-
-        # Ensure you call the parent class's save.
-        # .save() returns a User object.
-        user = super(CustomSignupForm, self).save(request)
-
-
-
-        # You must return the original result.
-        return user
+class ContactForm(forms.Form):
+    contact_name = forms.CharField(required=True)
+    contact_email = forms.EmailField(required=True)
+    subject = forms.CharField(required=True)
+    content = forms.CharField(
+        required=True,
+        widget=forms.Textarea
+    )
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields['contact_name'].label = "Seu nome:"
+        self.fields['contact_email'].label = "Seu email:"
+        self.fields['content'].label = "O que você gostaria de dizer?"
+        self.fields['subject'].label = "Assunto"
